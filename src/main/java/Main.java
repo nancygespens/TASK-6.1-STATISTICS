ede60f2e8175e8688208abed2f68d372fc7da94c
Anastasia Telesnitckaya <108212488+nancygespens@users.noreply.github.com>
2022-07-30 16:10:37 +0300

    .gitignore

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import ru.netology.stats.StatsService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StatsService service = new StatsService();
+        System.out.println(service.getMinSales(sales);
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldFindBetweenEnds() {
+        StatsService service = new StatsService();
+
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expectedMonth = 12;
+        int actualMonth = service.getMinSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+    }
+}
